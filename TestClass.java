09fd46cd74be33d3fdf71a2d4a8a2e0c116cd328
LAPTOP-AU0SMQI8\mohd adil <md7248699370@gmail.com>
2024-08-12 01:35:34 +0530

    fialed to print indices on intellij

--- TestClass.java
+++ TestClass.java
@@ -1,7 +1,21 @@
 public class TestClass {
-    public static void main(String[]args){
-        String str = " the sky is blue ";
-        ReverseWord rv = new ReverseWord();
-        rv.reverseWords(str);
+    public static void main(String[]args) {
+
+
+//        ReverseWord
+
+
+//        String str = " the sky is blue ";
+//        ReverseWord rv = new ReverseWord();
+//        rv.reverseWords(str);
+
+
+//      TwoSum
+
+        int[] nums = {2, 7, 11, 15};
+        int traget = 11;
+        TwoSum ts = new TwoSum();
+        ts.twoSum(nums, traget);
+
     }
 }
