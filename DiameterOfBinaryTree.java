029d63138eea760063e654048baf70550875bbb6
LAPTOP-AU0SMQI8\mohd adil <md7248699370@gmail.com>
2024-09-04 19:10:18 +0530

    .\DiameterOfBinaryTree.java

---
+++ DiameterOfBinaryTree.java
@@ -0,0 +1,23 @@
+
+
+public class DiameterOfBinaryTree {
+    int diameter =0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter;
+    }
+
+    int height(BalancedBinaryTree.TreeNode node){
+
+        if(node ==  null){
+            return 0;
+        }
+        int l_height = height(node.left);//2
+        int r_height = height(node.right);//1
+        int nodeDiameter = (l_height + r_height);
+        diameter = Math.max(diameter,nodeDiameter);
+
+        return Math.max(l_height, r_height)+1;
+    }
+}
+
